416f47385e1f26abed8bba68377f27fa2ec66c3a
Willio06 <74824158+Willio06@users.noreply.github.com>
2023-12-06 17:40:50 +0100

    Update AI_v2.py

--- AI_v2.py
+++ AI_v2.py
@@ -1,7 +1,13 @@
+import glob
 import math
+from operator import le
 import random
+from turtle import window_height
 from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm, Normalize
 import numpy as npy
+import pandas as pd
+import seaborn as sns
 PI = 3.14159265358979323846
 LEARNINGRATE = 0.1
 
@@ -48,7 +54,13 @@ def forwardProp(network):#voert forward prop uit op een netwerk(lijst van lagen)
 
 #bereken de costfunctie: voer forwardpropegation uit met de huidige weights & biases, bereken dan MSE,werkt
 def cost(network,TrainingData):
-    cost = 0
+    mean=0
+    for i in range(TrainingData.shape[0]):
+        mean+=TrainingData[i][1]
+    mean = mean/TrainingData.shape[0]
+
+    cost = 0#SSE
+    SStot=0
     for i in range(TrainingData.shape[0]):
         TrainingDataComponent = TrainingData[i]
         network[0].nodeValues = npy.array([[TrainingDataComponent[0]]])
@@ -56,18 +68,25 @@ def cost(network,TrainingData):
         #print(network[-1].nodeValues)
         #print(TrainingDataComponent[1])
         cost += (TrainingDataComponent[1]-network[-1].nodeValues[0][0])**2
+        SStot += (TrainingDataComponent[1] - mean)**2
+    Rsq = 1 - cost/SStot
     cost = cost/TrainingData.shape[0]
-    return cost
+    return [cost, Rsq]
 
 #1ste versie gradient descent: voor elke laag in het netwerk: bereken gradient numeriek met def afgeleide (f(x+h)-f(x))/h)
-def gradientDescent(trainingsdata,network):
-    originalCost = cost(network,trainingsdata)
-    #print("OriginalCost: ",end="")
-    #print(originalCost)
+def ADAM(trainingsdata,network,moments,epoch, adamBool,miniBatch):
+    npy.random.shuffle(trainingsdata)
+    datapoints = trainingsdata[:miniBatch]
+    b1=0.9
+    b2=0.999
+    eps=10**(-7)
+
+    originalCost = cost(network,datapoints)[0]
     h = 0.1
 
     GRADbias = []
     GRADweights = []
+
     for i in range(1,len(network)):
         gradientWeights = npy.zeros((network[i].outgoingNodes, network[i].incomingNodes))
         gradientBias = npy.zeros((network[i].outgoingNodes, 1))
@@ -75,13 +94,13 @@ def gradientDescent(trainingsdata,network):
         #bewerking is hier analoog aan bias maar dan in 2 dimensies
         for k in range(network[i].outgoingNodes):
             network[i].bias[k][0] += h #(x+h)
-            Cost = cost(network,trainingsdata)#f(x+h) 
+            Cost = cost(network,datapoints)[0]#f(x+h) 
             gradientBias[k][0] = ((Cost-originalCost)/h) #voeg def afgeleide toe aan originalCost
             network[i].bias[k][0] -=h #trek h er terug vanaf zodat deze geen impact heeft op volgende berekeningen
 
             for l in range(network[i].incomingNodes):
                 network[i].weights[k][l] +=h
-                Cost = cost(network,trainingsdata)
+                Cost = cost(network,datapoints)[0]
                 gradientWeights[k][l]=(Cost-originalCost)/h
                 network[i].weights[k][l] -=h     
         #print(gradientWeights)
@@ -89,52 +108,117 @@ def gradientDescent(trainingsdata,network):
         #print(network[i].bias)
         GRADbias.append(gradientBias)
         GRADweights.append(gradientWeights)
-         
-        #print(network[i].bias
-    for i in range(len(network)-1):
-        network[i+1].bias -= LEARNINGRATE * GRADbias[i]
-        network[i+1].weights -= LEARNINGRATE * GRADweights[i]
-    #print("newCost: ",end="")
-    #print(cost(network,trainingsdata))
+
+
+    if(adamBool):
+        for i in range(len(network)-1):
+            if(epoch==0):
+                moments[0].append((1-b1)*GRADbias[i])
+                moments[1].append((1-b2)*GRADbias[i]**2)
+                moments[2].append((1-b1)*GRADweights[i])
+                moments[3].append((1-b2)*GRADweights[i]**2)
+            else:
+                moments[0][i] = b1*moments[0][i]+(1-b1)*GRADbias[i]
+                moments[1][i] = b2*moments[1][i]+(1-b2)*GRADbias[i]**2
+                moments[2][i] = b1*moments[2][i]+(1-b1)*GRADweights[i]
+                moments[3][i] = b2*moments[3][i]+(1-b2)*GRADweights[i]**2
+            mbias = moments[0][i]/(1-b1**(epoch+int(epoch==0)))
+            vbias = moments[1][i]/(1-b2**(epoch+int(epoch==0)))
+            mweight = moments[2][i]/(1-b1**(epoch+int(epoch==0)))
+            vweight = moments[3][i]/(1-b2**(epoch+int(epoch==0))) 
+        
+            network[i+1].bias -= LEARNINGRATE * npy.multiply(mbias,1/(npy.sqrt(vbias)+eps))
+            network[i+1].weights -= LEARNINGRATE * npy.multiply(mweight,1/(npy.sqrt(vweight)+eps))
+    else:
+        for i in range(len(network)-1):
+            network[i+1].bias -= LEARNINGRATE * GRADbias[i]
+            network[i+1].weights -= LEARNINGRATE * GRADweights[i]
     return network
 
 #voor elk datapunt, forwardProp hiermee en voer dan gradientdescent uit
 def Learn(network,trainingsdata):
-    for k in range(200):
-        print("learn routine "+str(k))
-        network = gradientDescent(trainingsdata,network)
-    print("OriginalCost: ",end="")
-    print(cost(network,trainingsdata))
+    global LEARNINGRATE
+    moments=[[],[],[],[]]
+    x=npy.random.uniform(-1*PI,PI, trainingsdata.shape[0])
+    ValidationData = npy.column_stack((x,npy.sin(x)))
+    
+    TrainARRAY=[]
+    ValidationARRAY = [0]*100
+    ValidationERR=[]
+    for epoch in range(2000):
+        print("learn routine "+str(epoch)+" LearningRate: "+str(LEARNINGRATE))
+        #LEARNINGRATE = -0.1/2000*epoch+0.1
+        network = ADAM(trainingsdata,network,moments,epoch,True,10)
+        TrainARRAY.append(cost(network,trainingsdata)[0])
+        ValidationARRAY.append(cost(network,ValidationData)[0])
+        ValidationERR.append(cost(network,ValidationData)[0])
+        ValidationARRAY.pop(0)
+        
+        a= nearest_neighbor_mean(ValidationARRAY,5)
+        if(sorted(a)==a and epoch>100):
+            print("early stop at epoch: "+str(epoch))
+            break
+    plt.subplot(3,1,1)
+    plt.yscale("log")
+    plt.scatter(list(range(epoch+1)),TrainARRAY)
+    
+    plt.subplot(3,1,2)
+    plt.yscale("log")
+    plt.scatter(list(range(epoch+1)),ValidationERR)
+    
+    plt.subplot(3,1,3)
+    xvals = npy.random.uniform(-1*PI,PI, trainingsdata.shape[0])
+    yvals = []
+    #hier gebruiken we het netwerk om functiewaardes te benaderen: y = netwerk(x)
+    for i in range(trainingsdata.shape[0]):
+        #print(i)
+        network[0].nodeValues = npy.array([[xvals[i]]])
+        y = round(forwardProp(network)[-1].nodeValues[0][0],5)
+        yvals.append(y)
+    plt.yscale("linear")
+    plt.scatter(xvals,yvals)
+    plt.scatter(xvals, npy.sin(xvals))
+    plt.show()
+ 
 
-def main():
+def main(): 
     size = 10 #de grootte van de lagen in het midden
-    trainingsize = 250
-
+    
+    trainingsize = 100
     x=npy.random.uniform(-1*PI,PI, trainingsize)
     trainingsdata = npy.column_stack((x,npy.sin(x)))
     print(trainingsdata)
-    layersize = [1, size, size, 1]
+    
+    network = initialize_network([1, size, 1])
+
+    Learn(network,trainingsdata)
+    print("netwerk getraind")
+
+    #hier gebruiken we het netwerk om functiewaardes te benaderen: y = netwerk(x)
+    plt.show()
+
+
+
+def initialize_network(layersize):
     network=[layer(0,0,0,layersize[0],[[0]])]
     for i in range(1,len(layersize)):# aantal layers
         randweights = npy.array([[random.random() for i in range(network[i-1].outgoingNodes)] for _ in range(layersize[i])])
         randbias = npy.array([[random.random()] for _ in range(layersize[i])])
         network.append(layer(randweights, randbias, network[i-1].outgoingNodes, layersize[i], npy.zeros((layersize[i],1))))
-    
-    Learn(network,trainingsdata)
-    print("netwerk getraind")
+    return network
 
-    xvals = npy.random.uniform(-1*PI,PI, trainingsize)
-    yvals = []
-    #hier gebruiken we het netwerk om functiewaardes te benaderen: y = netwerk(x)
-    for i in range(trainingsize):
-        #print(i)
-        network[0].nodeValues = npy.array([[xvals[i]]])
-        y = round(forwardProp(network)[-1].nodeValues[0][0],5)
-        yvals.append(y)
-    #print(xvals)
-    #print(yvals)
-    plt.scatter(xvals,yvals)
-    plt.show()
+def nearest_neighbor_mean(array, windowsize):
+    meanList=[]
+    for i in range(len(array)):
+        if(i<math.floor(windowsize/2)):
+            segment = array[0:windowsize]
+        elif(i>math.floor(windowsize/2)):
+            segment = array[-windowsize:]
+        else:
+            segment = array[i-math.floor(windowsize/2):i+math.floor(windowsize/2)]
+        meanList.append(npy.mean(segment))
+    return meanList
+
 
 if __name__ =="__main__":
     main()
